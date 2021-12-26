15080501d49b725233535841bca553ce607e48e1
Murad Khan <muradali@miu.edu>
2021-12-25 23:30:31 -0600

    first commit

---
+++ src/main/java/com/example/dockerjinkins/DockerJinkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjinkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJinkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJinkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerjinkins/DockerJinkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjinkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJinkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
